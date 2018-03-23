e8ae92aac7e269367be77bc1086e7b19dc4bd067
mikeeus <itsmikias@gmail.com>
2018-03-23 23:01:48 +0300

    Add all users and scoreless btns and methods

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,33 +1,40 @@
 <template>
   <div class="container">
+    <v-progress-linear :indeterminate="true" v-if="loading">
+    </v-progress-linear>
+    <v-layout row>
+      <v-btn @click="fetchLeaderboard">Leaderboard</v-btn>
+      <v-btn @click="fetchUsers">All Users</v-btn>
+      <v-btn @click="fetchScoreless">Scoreless</v-btn>
+    </v-layout>
+    <!-- Sorting?? -->
     <v-list subheader>
       <v-subheader>Today's Leaderboard</v-subheader>
-      <v-progress-linear :indeterminate="true" v-if="loading">
-      </v-progress-linear>
-      <v-list-tile avatar v-for="(item, index) in items" v-bind:key="item.id" @click="clicked">
+
+      <v-list-tile avatar v-for="(user, index) in items" v-bind:key="user.id" @click="selectUser(user)">
         <v-list-tile-action>
           <div class="rank">{{ index + 1 }}</div>
         </v-list-tile-action>
         <v-list-tile-avatar>
-          <img :src="item.avatar">
+          <img :src="user.avatar">
         </v-list-tile-avatar>
         <v-list-tile-content>
-          <v-list-tile-title v-html="item.name">{{ item.name }}</v-list-tile-title>
+          <v-list-tile-title v-html="user.name">{{ user.name }}</v-list-tile-title>
         </v-list-tile-content>
         <v-list-tile-action class="score">
-          {{ item.daily_score }}
+          {{ user.daily_score }}
         </v-list-tile-action>
         <v-list-tile-action>
           <v-layout row>
-            <v-icon v-if="item.latest_change < 0"
+            <v-icon v-if="user.latest_change < 0"
               :color="'red'">
               arrow_downward
             </v-icon>
-            <v-icon v-if="item.latest_change > 0"
+            <v-icon v-if="user.latest_change > 0"
               :color="'green'">
               arrow_upward
             </v-icon>
-            <span>{{ item.latest_change }}</span>
+            <span>{{ user.latest_change }}</span>
           </v-layout>
         </v-list-tile-action>
       </v-list-tile>
@@ -41,28 +48,57 @@ import axios from 'axios'
 export default {
   name: 'Leaderboard',
   methods: {
-    fetchData () {
-      axios.get(this.apiUrl)
+    fetchLeaderboard () {
+      this.loading = true
+      axios.get(this.apiUrl + 'scores')
         .then(res => {
-          this.items = res.data
+          this.leaderboard = res.data
+          this.items = this.leaderboard
           this.loading = false
         }, err => {
           this.err = err
           this.loading = false
         })
     },
-    clicked () {
-      console.log('clicked')
+    fetchUsers () {
+      this.loading = true
+      axios.get(this.apiUrl + 'users')
+        .then(res => {
+          this.users = res.data
+          this.loading = false
+          this.items = this.users
+        }, err => {
+          this.err = err
+          this.loading = false
+        })
+    },
+    fetchScoreless () {
+      this.loading = true
+      axios.get(this.apiUrl + 'scoreless')
+        .then(res => {
+          this.scoreless = res.data
+          this.loading = false
+          this.items = this.scoreless
+        }, err => {
+          this.err = err
+          this.loading = false
+        })
+    },
+    selectUser (user) {
+      this.user = user
     }
   },
   created () {
-    this.loading = true
-    this.fetchData()
+    this.fetchLeaderboard()
   },
   data () {
     return {
-      apiUrl: 'https://saleskick-task-server.herokuapp.com/scores',
+      apiUrl: 'https://saleskick-task-server.herokuapp.com/',
       items: [],
+      leaderboard: [],
+      users: [],
+      scoreless: [],
+      user: null,
       err: null
       // id, name, avatar, daily_score, daily_score_count, latest_change
     }
