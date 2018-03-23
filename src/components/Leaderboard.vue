cbf96965dd77b3b3ef76005ace7aa2e8b9b90253
mikeeus <itsmikias@gmail.com>
2018-03-24 00:36:17 +0300

    Add user detail. Add styling. Refactor

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -2,15 +2,52 @@
   <div class="container">
     <v-progress-linear :indeterminate="true" v-if="loading">
     </v-progress-linear>
+
     <v-layout row>
       <v-btn @click="fetchLeaderboard">Leaderboard</v-btn>
       <v-btn @click="fetchUsers">All Users</v-btn>
       <v-btn @click="fetchScoreless">Scoreless</v-btn>
     </v-layout>
-    <!-- Sorting?? -->
-    <v-list subheader>
-      <v-subheader>Today's Leaderboard</v-subheader>
 
+    <v-card color="primary darken-2"
+      v-if="user" class="user-detail white--text">
+      <v-container fluid grid-list-lg>
+        <v-layout row>
+          <img :src="user.avatar" alt="Profile picture"
+            height="125px"
+            width="125px">
+          <v-flex>
+            <div>
+              <v-layout row>
+                <div class="headline">{{ user.name }}</div>
+                <div>{{ user.daily_score }}</div>
+              </v-layout>
+              <v-layout column>
+                <ul class="user-score">
+                  <li v-for="score in user.scores" v-bind:key="score.id">
+                    <v-layout row>
+                      <v-icon v-if="score.value < 0"
+                        :color="'red'">
+                        arrow_downward
+                      </v-icon>
+                      <v-icon v-if="score.value > 0"
+                        :color="'green'">
+                        arrow_upward
+                      </v-icon>
+                      <strong>{{ score.value }}</strong>
+                      <span>{{ score.created_at | moment('from', 'now') }}</span>
+                    </v-layout>
+                  </li>
+                </ul>
+              </v-layout>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+
+    <v-list subheader>
+      <v-subheader>{{ viewed }}</v-subheader>
       <v-list-tile avatar v-for="(user, index) in items" v-bind:key="user.id" @click="selectUser(user)">
         <v-list-tile-action>
           <div class="rank">{{ index + 1 }}</div>
@@ -26,15 +63,15 @@
         </v-list-tile-action>
         <v-list-tile-action>
           <v-layout row>
-            <v-icon v-if="user.latest_change < 0"
+            <v-icon v-if="user.latest_score < 0"
               :color="'red'">
               arrow_downward
             </v-icon>
-            <v-icon v-if="user.latest_change > 0"
+            <v-icon v-if="user.latest_score > 0"
               :color="'green'">
               arrow_upward
             </v-icon>
-            <span>{{ user.latest_change }}</span>
+            <span>{{ user.latest_score }}</span>
           </v-layout>
         </v-list-tile-action>
       </v-list-tile>
@@ -48,44 +85,62 @@ import axios from 'axios'
 export default {
   name: 'Leaderboard',
   methods: {
-    fetchLeaderboard () {
+    startLoading() {
       this.loading = true
+    },
+    stopLoading() {
+      this.loading = false
+    },
+    handleError(err) {
+      this.err = err
+      this.stopLoading()
+    },
+    fetchLeaderboard () {
+      this.startLoading()
       axios.get(this.apiUrl + 'scores')
         .then(res => {
           this.leaderboard = res.data
           this.items = this.leaderboard
-          this.loading = false
+          this.viewed = 'Leaderboard'
+          this.stopLoading()
         }, err => {
-          this.err = err
-          this.loading = false
+          this.handleError(err)
         })
     },
     fetchUsers () {
-      this.loading = true
+      this.startLoading()
       axios.get(this.apiUrl + 'users')
         .then(res => {
           this.users = res.data
-          this.loading = false
           this.items = this.users
+          this.viewed = 'All Users'
+          this.stopLoading()
         }, err => {
-          this.err = err
-          this.loading = false
+          this.handleError(err)
         })
     },
     fetchScoreless () {
-      this.loading = true
+      this.startLoading()
       axios.get(this.apiUrl + 'scoreless')
         .then(res => {
           this.scoreless = res.data
-          this.loading = false
           this.items = this.scoreless
+          this.viewed = 'Scoreless'
+          this.stopLoading()
         }, err => {
-          this.err = err
-          this.loading = false
+          this.handleError(err)
         })
     },
     selectUser (user) {
       this.user = user
+      axios.get(this.apiUrl + 'users/' + user.id)
+        .then(res => {
+          this.user = res.data.user
+          this.user.scores = res.data.scores
+          this.stopLoading()
+        }, err => {
+          this.handleError(err)
+        })
     }
   },
   created () {
@@ -93,14 +148,16 @@ export default {
   },
   data () {
     return {
-      apiUrl: 'https://saleskick-task-server.herokuapp.com/',
+      // apiUrl: 'https://saleskick-task-server.herokuapp.com/',
+      apiUrl: 'http://localhost:3000/',
       items: [],
       leaderboard: [],
       users: [],
       scoreless: [],
       user: null,
+      viewed: '',
       err: null
-      // id, name, avatar, daily_score, daily_score_count, latest_change
+      // id, name, avatar, daily_score, daily_score_count, latest_score
     }
   }
 }
@@ -115,5 +172,28 @@ export default {
 }
 .score {
   font-size: 48px;
+}
+.user-detail {
+  margin-bottom: 15px;
+  img {
+    border-radius: 50%;
+    margin: 15px 30px;
+    padding: 1px;
+    border: 2px solid;
+    box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.3);
+  }
+}
+.headline {
+  font-weight: bold;
+}
+.user-score {
+  list-style: none;
+  li {
+    padding: 10px;
+    strong {
+      padding: 0 5px;
+      width: 50px;
+    }
+  }
 }
 </style>
